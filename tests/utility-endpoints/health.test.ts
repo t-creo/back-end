9b63c5aef2843ac93944d999e83a3949e3fa9b9c
German Robayo <germanrobayo33@gmail.com>
2019-09-23 15:33:58 -0400

    add first test on the platform

--- tests/utility-endpoints/health.test.ts
+++ tests/utility-endpoints/health.test.ts
@@ -1,5 +1,12 @@
+import request from 'supertest'
+import app from '../../src/app'
+
 describe('/health endpoint', () => {
   test('returns http status code 200', () => {
-    
+    return request(app)
+      .get('/health')
+      .expect(200, {
+        status: 'UP'
+      })
   })
 })
