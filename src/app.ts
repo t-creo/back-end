d0574621c5e4d6235865146bbfc0a99d1589ad3f
German Robayo <germanrobayo33@gmail.com>
2019-09-26 21:43:56 -0400

    use no-unused-vars from typescript plugin instead of eslint native impl

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import calculatorRoutes from './calculator/routes'
 const app = express()
 
 app.use('/health', (req, res) => {
   res.json({ status: 'UP' })
 })
 
+app.use('/calculate/pl')
 export default app
