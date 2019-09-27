a40d3e376f5b5826fcf389585c50237c9381e2e6
German Robayo <germanrobayo33@gmail.com>
2019-09-27 00:20:19 -0400

    add tests for only badwords criteria

--- tests/calculus-endpoints/plain-text.test.ts
+++ tests/calculus-endpoints/plain-text.test.ts
@@ -1,15 +1,41 @@
 import request from 'supertest'
 import app from '../../src/app'
-import { Credibility } from '../../src/calculator/models'
+import { Credibility, TextCredibilityWeights } from '../../src/calculator/models'
+
+interface PlainTextQueryParams extends TextCredibilityWeights {
+  text: string
+}
 
 describe('/calculate/plain-text endpoint', () => {
-  test('returns http status code 200 on valid input', () => {
-    const expectedReturn : Credibility = {
-      credibility: 1
-    }
-    return request(app)
-      .get('/calculate/plain-text')
-      .expect(200)
-      .expect(expectedReturn)
+  describe('http 200 calls', () => {
+    describe('full bad criteria', () => {
+      function testCredibilityWithOkData(
+        expectedReturn : Credibility,textToAnalyze: string) {
+        const textCredibilityWeights : PlainTextQueryParams = {
+          weightBadWords: 1,
+          weightMisspelling: 0,
+          weightSpam: 0,
+          text: textToAnalyze
+        }
+        return request(app)
+          .get('/calculate/plain-text')
+          .query(textCredibilityWeights)
+          .expect(200)
+          .expect(expectedReturn)
+
+      }
+      it('returns credibility=100 on full bad words filtering with no bad words', () => {
+        return testCredibilityWithOkData({ credibility: 100 }, 'yes no')
+      })
+
+      it('returns credibility=50 with 2 words and 1 bad', () => {
+        return testCredibilityWithOkData({ credibility: 50 }, 'yes hell')
+      })
+
+      it('returns credibility=0 with 2 words and 2 bad words', () => {
+        return testCredibilityWithOkData({ credibility: 0 }, 'hell hell')
+      })
+
+    })
   })
 })
