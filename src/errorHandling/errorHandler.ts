ef0d593eefa3aa3653d09d608906e11c3d30f21b
Jose Acevedo <13-10006@usb.ve>
2019-09-27 23:09:17 +0200

    Add handling of error codes

--- src/errorHandling/errorHandler.ts
+++ src/errorHandling/errorHandler.ts
@@ -1,3 +1,13 @@
 import * as ErrorTypes from './errorTypes'
 
-export default class CustomError extends Error {}
+export default function errorHandler(errorType : string) {
+
+  switch(errorType) {
+  case '400': { return ErrorTypes.error400() }
+  case '401': { return ErrorTypes.error401() }
+  case '403': { return ErrorTypes.error403() }
+  case '500': { return ErrorTypes.error500() }
+  default   : { return ErrorTypes.invalidError() }
+  }
+
+}
