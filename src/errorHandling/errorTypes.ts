a865df09035b242041429cc636298065afa0e94f
Jose Acevedo <13-10006@usb.ve>
2019-09-27 23:08:36 +0200

    Add handling of invalid code and refactor

--- src/errorHandling/errorTypes.ts
+++ src/errorHandling/errorTypes.ts
@@ -1,31 +1,46 @@
-import ErrorObjectInterface from './errorObjectInterface'
-
-/* Client-side Errors */
-export class Error400 extends Error implements ErrorObjectInterface {
-    status      = 400;
-    title       = "Bad Request";
-    message     = "A validation failed";
-    userMessage = "An error has ocurred";
+/* Client-side Errors */ 
+export function error400() {
+  return {
+    status      : 400,
+    title       : 'Bad Request',
+    message     : 'A validation failed',
+    userMessage : 'An error has ocurred',
+  }
 }
 
-export class Error401 extends Error implements ErrorObjectInterface {
-    status      = 401;
-    title       = "Unauthorized";
-    message     = "Not authenticated";
-    userMessage = "Client needs to authenticate";
+export function error401() {
+  return {
+    status      : 401,
+    title       : 'Unauthorized',
+    message     : 'Not authenticated',
+    userMessage : 'Client needs to authenticate',
+  }
 }
 
-export class Error403 extends Error implements ErrorObjectInterface {
-    status      = 403;
-    title       = "Forbidden";
-    message     = "Cannot Access";
-    userMessage = "Client cannot access this resource";
+export function error403() {
+  return {
+    status      : 403,
+    title       : 'Forbidden',
+    message     : 'Cannot Access',
+    userMessage : 'Client cannot access this resource',
+  }
 }
 
 /* Server-side Errors */
-export class Error500 extends Error implements ErrorObjectInterface {
-    status      = 500;
-    title       = "Internal Server Error";
-    message     = "An error has ocurred";
-    userMessage = "An error has ocurred";
+export function error500() {
+  return {
+    status      : 500,
+    title       : 'Internal Server Error',
+    message     : 'An error has ocurred',
+    userMessage : 'An error has ocurred',
+  }
+}
+
+export function invalidError() {
+  return {
+    status      : -1,
+    title       : 'Invalid Error Code',
+    message     : 'Invalid error code',
+    userMessage : 'Invalid error code',
+  }
 }
