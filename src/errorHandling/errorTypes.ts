4ada9b8bbd823d183b24505418902ec253bf9063
Jose Acevedo <13-10006@usb.ve>
2019-09-27 20:13:52 +0200

    Add Error 500 class

--- src/errorHandling/errorTypes.ts
+++ src/errorHandling/errorTypes.ts
@@ -23,4 +23,9 @@ export class Error403 extends Error implements ErrorObjectInterface {
 }
 
 /* Server-side Errors */
-export class Error500 extends Error implements ErrorObjectInterface {}
+export class Error500 extends Error implements ErrorObjectInterface {
+    status      = 500;
+    title       = "Internal Server Error";
+    message     = "An error has ocurred";
+    userMessage = "An error has ocurred";
+}
