8d6dd4a8b8b86e42580bd2db5d4a61c86075d5cb
David Cabeza <cabezadavide@gmail.com>
2019-09-28 13:54:56 +0200

    Add social credibility calculator route

--- src/calculator/routes.ts
+++ src/calculator/routes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { textCredibility } from './service'
+import { textCredibility, socialCredibility } from './service'
 
 const calculatorRoutes = express.Router()
 
@@ -9,6 +9,10 @@ calculatorRoutes.get('/plain-text', function(req, res) {
     weightMisspelling: +req.query.weightMisspelling,
     weightSpam: +req.query.weightSpam
   }))
+})
+
+calculatorRoutes.get('/twitter/social/:userID', function(req, res){
+  res.send(socialCredibility(req.params.userID))
 })
 
 export default calculatorRoutes
