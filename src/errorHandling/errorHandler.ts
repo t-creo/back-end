a54b63a7169fa874e49ace1d650970f6b82a5c08
Jose Acevedo <13-10006@usb.ve>
2019-09-29 02:25:20 +0200

    Refactor Error Handler

--- src/errorHandling/errorHandler.ts
+++ src/errorHandling/errorHandler.ts
@@ -1,13 +1,10 @@
-import * as ErrorTypes from './errorTypes'
+import { NextFunction, Request, Response } from 'express' // eslint-disable-line no-unused-vars
+import HttpError from './httpError' // eslint-disable-line no-unused-vars
 
-export default function errorHandler(errorType : string) {
-
-  switch(errorType) {
-  case '400': { return ErrorTypes.error400() }
-  case '401': { return ErrorTypes.error401() }
-  case '403': { return ErrorTypes.error403() }
-  case '500': { return ErrorTypes.error500() }
-  default   : { return ErrorTypes.invalidError() }
-  }
-
+function errorHandler(error: HttpError, request: Request, response: Response, next: NextFunction) { // eslint-disable-line no-unused-vars
+  response
+    .status(error.status)
+    .send(error)
 }
+
+export default errorHandler
