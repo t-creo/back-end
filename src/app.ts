82a5474e4e5d226d558fd81a1e47d557ea9461e1
Jose Acevedo <13-10006@usb.ve>
2019-09-29 02:25:43 +0200

    Add Error Express Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import errorHandler from './errorHandling/errorHandler'
+
 const app = express()
 
 app.use('/health', (req, res) => {
   res.json({ status: 'UP' })
 })
+
+app.use(errorHandler)
 
 export default app
