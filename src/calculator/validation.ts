a3c866a50aea62044591c261d7f2a54c08e94f82
David Cabeza <cabezadavide@gmail.com>
2019-10-06 23:22:52 +0200

    Fix missing bracket

--- src/calculator/validation.ts
+++ src/calculator/validation.ts
@@ -38,7 +38,6 @@ export function validate(method: string) : any {
       check('friendsCount', 'friendsCount.REQUIRED').exists(),
     ]
   }
-  
   case 'scrapperTwitterUserCredibility': {
     return [
       check('verified', 'verified.REQUIRED').exists(),
@@ -47,6 +46,7 @@ export function validate(method: string) : any {
       check('accountCreationYear', 'accountCreationYear.NUMBER').isInt(),
       check('accountCreationYear', 'accountCreationYear.NOT_IN_RANGE').isInt({min: 2006, max: 2019})
     ]
+  }
   }
 }
 
