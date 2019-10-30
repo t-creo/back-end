c76e072997d4ec16c53362151d6e6627576c1b4d
German Robayo <germanrobayo33@gmail.com>
2019-10-30 10:51:44 -0400

    refactor

--- src/calculator/routes.ts
+++ src/calculator/routes.ts
@@ -2,40 +2,32 @@ import express from 'express'
 import { calculateTextCredibility, socialCredibility, twitterUserCredibility, calculateTweetCredibility, scrapperTwitterUserCredibility, scrapedSocialCredibility, scrapedtweetCredibility} from './service'
 import { validationResult } from 'express-validator'
 import { validate, errorMapper } from './validation'
+import { asyncWrap } from '../utils'
 
 const calculatorRoutes = express.Router()
 
-calculatorRoutes.get('/plain-text', validate('calculateTextCredibility'), function(req, res, next) {
+calculatorRoutes.get('/plain-text', validate('calculateTextCredibility'), asyncWrap(async function(req, res) {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     errorMapper(errors.array())
   }
-  console.time('testing plain-text')
-  calculateTextCredibility({
+  res.json(await calculateTextCredibility({
     text: req.query.text,
     lang: req.query.lang
   }, {
     weightBadWords: +req.query.weightBadWords,
     weightMisspelling: +req.query.weightMisspelling,
     weightSpam: +req.query.weightSpam
-  }).then(credibility => {
-    res.send(credibility)
-    console.timeEnd('testing plain-text')
-    next()
-  })
-})
+  }))
+}))
 
-calculatorRoutes.get('/twitter/user/:id', validate('twitterUserCredibility'), function(req, res, next) {
+calculatorRoutes.get('/twitter/user/:id', validate('twitterUserCredibility'), asyncWrap(async function(req, res) {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     errorMapper(errors.array())
   }
-  twitterUserCredibility(req.params.id)
-    .then(response => {
-      res.send(response)
-      next()
-    })
-})
+  res.json(await twitterUserCredibility(req.params.id))
+}))
 
 calculatorRoutes.get('/user/scrape', validate('scrapperTwitterUserCredibility'), function(req, res) {
   const errors = validationResult(req)
@@ -46,33 +38,29 @@ calculatorRoutes.get('/user/scrape', validate('scrapperTwitterUserCredibility'),
   res.send(userCredibility)
 })
 
-calculatorRoutes.get('/twitter/social/:userId', validate('socialCredibility'), async function(req, res) {
+calculatorRoutes.get('/twitter/social/:userId', validate('socialCredibility'), asyncWrap(async function(req, res) {
   const errors = validationResult(req)
   if (!errors.isEmpty()){
     errorMapper(errors.array())
   }
   const socialCredibilityVal = await socialCredibility(req.params.userId, req.query.maxFollowers)
   res.send(socialCredibilityVal)
-})
+}))
 
-calculatorRoutes.get('/twitter/tweets', validate('tweetCredibility'), function(req, res, next) {
+calculatorRoutes.get('/twitter/tweets', validate('tweetCredibility'), asyncWrap(async function(req, res) {
   const errors = validationResult(req)
   if (!errors.isEmpty()){
     errorMapper(errors.array())
   }
-  calculateTweetCredibility(req.query.tweetId, {
+  res.json(await calculateTweetCredibility(req.query.tweetId, {
     weightBadWords: +req.query.weightBadWords,
     weightMisspelling: +req.query.weightMisspelling,
     weightSpam: +req.query.weightSpam,
     weightSocial: +req.query.weightSocial,
     weightText: +req.query.weightText,
-    weightUser: +req.query.weightUser }, 
-  req.query.maxFollowers)
-    .then(response => {
-      res.send(response)
-      next()
-    })
-})
+    weightUser: +req.query.weightUser },
+  req.query.maxFollowers))
+}))
 
 calculatorRoutes.get('/social/scrape', validate('scrapedSocialCredibility'), function(req, res){
   const errors = validationResult(req)
@@ -82,12 +70,12 @@ calculatorRoutes.get('/social/scrape', validate('scrapedSocialCredibility'), fun
   res.send(scrapedSocialCredibility(req.query.followersCount, req.query.friendsCount, req.query.maxFollowers))
 })
 
-calculatorRoutes.get('/tweets/scraped', validate('scrapedTweetCredibility'), function(req, res, done){
+calculatorRoutes.get('/tweets/scraped', validate('scrapedTweetCredibility'), asyncWrap(async function(req, res){
   const errors = validationResult(req)
   if (!errors.isEmpty()){
     errorMapper(errors.array())
   }
-  scrapedtweetCredibility({
+  res.json(await scrapedtweetCredibility({
     text: req.query.tweetText,
     lang: req.query.lang
   }, {
@@ -104,11 +92,7 @@ calculatorRoutes.get('/tweets/scraped', validate('scrapedTweetCredibility'), fun
     yearJoined: +req.query.yearJoined,
     followersCount: +req.query.followersCount,
     friendsCount: +req.query.friendsCount
-  },
-  req.query.maxFollowers).then(credibility => {
-    res.send(credibility)
-    done()
-  })
-})
+  }, req.query.maxFollowers))
+}))
 
 export default calculatorRoutes
--- src/errorHandling/httpError.ts
+++ src/errorHandling/httpError.ts
@@ -1,53 +1,69 @@
-import { ErrorObjectInterface, ErrorDetailInterface } from './errorObjectInterface' // eslint-disable-line no-unused-vars
+/* istanbul ignore file */
 
-class HttpError extends Error implements ErrorObjectInterface {
+import {ErrorDetailInterface, ErrorObjectInterface} from './errorObjectInterface' // eslint-disable-line no-unused-vars
 
-  status: number;
-  title: string;
-  message: string;
-  userMessage: string;
+export default abstract class HttpError extends Error implements ErrorObjectInterface {
+  status: number = 0
+  title: string = ''
+  message: string = ''
+  userMessage: string = ''
   errors?: ErrorDetailInterface[]
-
-  constructor(status: number, errors?: ErrorDetailInterface[]) {
+  constructor() {
     super()
+    Object.setPrototypeOf(this, HttpError.prototype)
+  }
+}
 
-    this.status = 0
-    this.title = ''
-    this.message = ''
-    this.userMessage = ''
-
-    switch(status) {
-    case 400: { 
-      this.status = 400
-      this.title = 'Bad Request'
-      this.message = 'A validation failed'
-      this.userMessage = 'An error has ocurred'
+export class BadRequestError extends HttpError {
+  constructor(errors: ErrorDetailInterface[] = []) {
+    super()
+    this.status = 400
+    this.title = 'Bad Request'
+    this.message = 'A validation failed or the request was bad formatted'
+    this.userMessage = 'A validation failed'
+    if (errors.length !== 0) {
       this.errors = errors
-      break
-    }
-    case 401: { 
-      this.status = 401
-      this.title = 'Unauthorized'
-      this.message = 'Not authenticated'
-      this.userMessage = 'Client needs to authenticate'
-      break
-    } 
-    case 403: { 
-      this.status = 403
-      this.title = 'Forbidden'
-      this.message = 'Cannot Access'
-      this.userMessage = 'Client cannot access this resource'
-      break
-    }
-    case 500: { 
-      this.status = 500
-      this.title = 'Internal Server Error'
-      this.message = 'An error has ocurred'
-      this.userMessage = 'An error has ocurred'
-      break
-    }
     }
   }
 }
- 
-export default HttpError
+
+export class UnauthenticatedError extends HttpError {
+  constructor() {
+    super()
+    this.status = 401
+    this.title = 'Unauthenticated'
+    this.message = 'Not authenticated'
+    this.userMessage = 'Client needs to authenticate'
+  }
+}
+
+export class UnauthorizedError extends HttpError {
+  constructor() {
+    super()
+    this.status = 403
+    this.title = 'Forbidden'
+    this.message = 'Cannot Access'
+    this.userMessage = 'Client cannot access this resource'
+  }
+}
+
+export class NotFoundError extends HttpError {
+  constructor() {
+    super()
+    this.status = 404
+    this.title = 'Not found'
+    this.message = 'The requested resource was not found'
+    this.userMessage = 'Not found'
+  }
+}
+
+
+export class InternalServerError extends HttpError {
+  constructor() {
+    super()
+    this.status = 500
+    this.title = 'Internal Server Error'
+    this.message = 'An error has ocurred'
+    this.userMessage = 'An error has ocurred'
+  }
+}
